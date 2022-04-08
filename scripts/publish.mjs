ca85ab9dc1b5faf3ad1dadb42280679ada456298
Mark Martirosian <hello@markmartirosian.com>
2022-04-08 19:54:49 +0000

    feat: release

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -11,31 +11,6 @@ const error = (message) => {
   process.exit(1)
 }
 
-// Options:
-//       --access <public|restricted>  Tells the registry whether this package should
-//                                     be published as public or restricted
-//       --dry-run                     Does everything a publish would do except
-//                                     actually publishing to the registry
-//       --force                       Packages are proceeded to be published even if
-//                                     their current version is already in the
-//                                     registry. This is useful when a
-//                                     "prepublishOnly" script bumps the version of
-//                                     the package before it is published
-//       --ignore-scripts              Ignores any publish related lifecycle scripts
-//                                     (prepublishOnly, postpublish, and the like)
-//       --no-git-checks               Don't check if current branch is your publish
-//                                     branch, clean, and up-to-date
-//       --otp                         When publishing packages that require
-//                                     two-factor authentication, this option can
-//                                     specify a one-time password
-//       --publish-branch              Sets branch name to publish. Default is master
-//       --report-summary              Save the list of the newly published packages
-//                                     to "pnpm-publish-summary.json". Useful when
-//                                     some other tooling is used to report the list
-//                                     of published packages.
-//       --tag <tag>                   Registers the published package with the given
-//                                     tag. By default, the "latest" tag is used.
-
 async function main() {
   const args = arg({
     '--dry-run': Boolean
@@ -53,29 +28,43 @@ async function main() {
 
   version = semver.clean(version)
 
-  const { name, version: currentVersion } = await fse.readJson(
-    path.join(process.cwd(), 'package.json')
-  )
+  const packageJsonPath = path.join(process.cwd(), 'package.json')
 
-  if (currentVersion === version) {
+  const packageJson = await fse.readJson(packageJsonPath)
+
+  if (packageJson.version === version) {
     error(
-      `Package version from "${version}" matches the current version "${currentVersion}"`
+      `Package version from "${version}" matches the current version "${packageJson.version}"`
     )
   }
+
+  fse.writeJSON(packageJsonPath, { ...packageJson, version })
+
+  await execa('pnpm', [
+    'exec',
+    'syncpack',
+    'format',
+    '--source',
+    packageJsonPath
+  ])
 
   await execa(
     'pnpm',
     [
       'publish',
+      '--no-git-checks',
       '--access',
       'public',
       '--publish-branch',
       'trunk',
       args['--dry-run'] ? '--dry-run' : undefined
-    ].filter((value) => value !== undefined)
+    ].filter((value) => value !== undefined),
+    {
+      env: {
+        NPM_TOKEN: process.env.NPM_TOKEN
+      }
+    }
   )
-
-  // console.log(name, version)
 }
 
 main()
